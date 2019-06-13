11297de2e3b376596b0ed6d6b709b43308f15594
Dean Rowley <deanrowley@users.noreply.github.com>
2019-06-14 00:24:15 +0100

    Added test Hello.cs class

---
+++ ConsoleAppGitHubTest001/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleAppGitHubTest001
+{
+    class Hello
+    {
+        string name;
+
+        public Hello(string name)
+        {
+            this.name = name;
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
--- ConsoleAppGitHubTest001/Program.cs
+++ ConsoleAppGitHubTest001/Program.cs
@@ -9,6 +9,11 @@ namespace ConsoleAppGitHubTest001
             // Just a hello world
             // Added in GitHub via webbrowser
             Console.WriteLine("Hello, World!");
+
+            Hello h = new Hello("Bob");
+
+            h.SayHello();
+
             Console.Write("Press a key to end");
             Console.ReadKey();
         }
