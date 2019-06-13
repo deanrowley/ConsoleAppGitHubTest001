8b484cbe0574b0f6ecd7482a9441ba8f29484d6d
Dean Rowley <deanrowley@users.noreply.github.com>
2019-06-13 23:52:26 +0100

    Update Program.cs

--- ConsoleAppGitHubTest001/Program.cs
+++ ConsoleAppGitHubTest001/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppGitHubTest001
         static void Main(string[] args)
         {
             // Just a hello world
+            // Added in GitHub via webbrowser
             Console.WriteLine("Hello, World!");
             Console.Write("Press a key to end");
             Console.ReadKey();
