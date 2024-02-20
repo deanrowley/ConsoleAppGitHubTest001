87e53521e8db9a1670c460273445d25f2927efb3
deanrowley <6888225+deanrowley@users.noreply.github.com>
2024-02-20 15:15:15 +0000

    Test Commit 2

--- ConsoleAppGitHubTest001/Program.cs
+++ ConsoleAppGitHubTest001/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleAppGitHubTest001
         {
             // Just a hello world
             // Added in GitHub via webbrowser
+            // 2024-02-20 Changed to test Visual Studio
             Console.WriteLine("Hello, World!");
 
             Hello h = new Hello("Bob");
